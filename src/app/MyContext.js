bf2642739657980fa6d9aab8666b692b7c89385a
AaryanDahal17 <malleckmelon@gmail.com>
2023-07-03 20:55:35 +0545

    some more changes

--- src/app/MyContext.js
+++ src/app/MyContext.js
@@ -4,14 +4,8 @@ import { createContext, useEffect, useState } from 'react';
 
 import { gettheItems } from './dbinteraction';
 
-// Create the context
 export const MyContext = createContext();
 
-
-
-
-
-// Create a provider component
 
 export const MyContextProvider = ({ children }) => {
 
@@ -30,8 +24,6 @@ export const MyContextProvider = ({ children }) => {
   },[])
     
 
-
-  // Define any functions or state values you want to make available through the context
 
   return (
     <MyContext.Provider value={{ theItems, setTheItems }}>
