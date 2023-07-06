b0fbc1028ee565bee50062966881de4e04dd57b2
AaryanDahal17 <malleckmelon@gmail.com>
2023-07-06 13:37:08 +0545

    changes to API Routes

--- src/app/dbinteraction.js
+++ src/app/dbinteraction.js
@@ -1,8 +1,8 @@
 async function gettheItems(){
 
-    const resp = await fetch("https://moneyleftgive.netlify.app/api/getitems",{cache:"no-store"})
+    // const resp = await fetch("https://moneyleftgive.netlify.app/api/getitems",{cache:"no-store"})
     // const resp = await fetch("https://food-loan-tracking.vercel.app/api/getitems", {cache:"no-store"})
-    // const resp = await fetch("http://localhost:3000/api/getitems", {cache:"no-store"})
+    const resp = await fetch("/api/getitems", {cache:"no-store"})
 
     const data = await resp.json()
 
@@ -11,9 +11,9 @@ async function gettheItems(){
 
 
 async function addtheitem(obj){
-    const res = await fetch('https://moneyleftgive.netlify.app/api/getitems',{
+    // const res = await fetch('https://moneyleftgive.netlify.app/api/getitems',{
     // const res = await fetch('https://food-loan-tracking.vercel.app/api/additem', {
-    // const res = await fetch('http://localhost:3000/api/additem', {
+    const res = await fetch('/api/additem', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
